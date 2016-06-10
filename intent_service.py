c25c0bd35d3a6b48b3544b74a00e5e40e0089b83
Maxime Bedard <maxime.bedard@shopify.com>
2016-06-10 11:06:01 -0400

    WERWER

--- intent_service.py
+++ intent_service.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
 def comment_intent():
-  body = request.args.get('body', '')
+  print(request.form)
+  body = request.form.get('body', '')
   intent = classifier.intent(body)
+  print(body)
+  print(intent)
   return intent
 
 if __name__ == '__main__':
